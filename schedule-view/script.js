794baa83c4b9c500088c9f1d8cbf811abac3923f
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2015-02-17 15:45:18 -0500

    Using Github Pages

---
+++ schedule-view/script.js
@@ -0,0 +1,78 @@
+Itinerary.directive('scheduleView', ['courseList', function(courseList) {
+    return {
+        templateUrl: 'schedule-view/template.html',
+        link: function(scope, element, attrs) {
+            // test data
+            scope.courses = courseList;
+            
+            // storage for "new" course
+            scope.newCourse = {
+                name: undefined,
+                startTime: undefined,
+                endTime: undefined,
+                days: [
+                    {
+                        'symbol': 'M',
+                        'value': false
+                    }, 
+                    {
+                        'symbol': 'T',
+                        'value': false
+                    }, 
+                    {
+                        'symbol':'W',
+                        'value': false
+                    }, 
+                    {
+                        'symbol': 'R',
+                        'value': false
+                    }, 
+                    {
+                        'symbol':'F',
+                        'value': false
+                    }
+                ],
+                viewColor: undefined
+            };
+            
+            scope.addCourse = function() {
+                if (scope.newCourse.name && scope.newCourse.startTime && scope.newCourse.endTime) {
+                    scope.courses.push(scope.newCourse);
+                    console.log(courseList);
+                    scope.newCourse = {
+                        name: undefined,
+                        startTime: undefined,
+                        endTime: undefined,
+                        days: [
+                            {
+                                'symbol': 'M',
+                                'value': false
+                            }, 
+                            {
+                                'symbol': 'T',
+                                'value': false
+                            }, 
+                            {
+                                'symbol':'W',
+                                'value': false
+                            }, 
+                            {
+                                'symbol': 'R',
+                                'value': false
+                            }, 
+                            {
+                                'symbol':'F',
+                                'value': false
+                            }
+                        ],
+                        viewColor: undefined
+                    };
+                }
+            }
+            
+            scope.removeCourse = function(index) {
+                scope.courses.splice(index, 1);
+            }
+        }
+    }
+}]);
